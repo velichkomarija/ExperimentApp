17380b83005315ec4729061afd7f1ec7f6a6da50
Maria Velichko <velichkomarija4@gmail.com>
2025-04-05 14:50:40 +0300

    Change colors

--- app/src/main/java/com/velichkomarija/everydaykit/todo/TasksItems.kt
+++ app/src/main/java/com/velichkomarija/everydaykit/todo/TasksItems.kt
@@ -14,10 +14,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextDecoration
@@ -79,7 +81,8 @@ private fun TasksEmptyContent(
         Image(
             painter = painterResource(id = noTasksIconRes),
             contentDescription = "",
-            modifier = Modifier.size(96.dp)
+            modifier = Modifier.size(96.dp),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
         )
         Text(stringResource(id = noTasksLabel))
     }
